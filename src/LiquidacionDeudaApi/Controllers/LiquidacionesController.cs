c8b279511fb1d0fee90b24a7900016c79584d3f2
josez <jose.calderon@cignium.com>
2020-03-26 10:05:41 -0500

    incluye referencia a sdk y apunta a servicio TecnocomCeco005

--- src/LiquidacionDeudaApi/Controllers/LiquidacionesController.cs
+++ src/LiquidacionDeudaApi/Controllers/LiquidacionesController.cs
@@ -9,6 +9,8 @@ using System.Web.Http;
 using LiquidacionDeudaApi.Aplicacion;
 using LiquidacionDeudaApi.Infraestructura.Servicios;
 using LiquidacionDeudaApi.ViewModels;
+using TecnocomWrapper.unicard.sdk.Clients;
+using TecnocomWrapper.unicard.sdk.PrePagoDiezDias;
 
 namespace LiquidacionDeudaApi.Controllers
 {
@@ -20,25 +22,30 @@ namespace LiquidacionDeudaApi.Controllers
             ResponseLiquidacion resposneLiquidacion = new ResponseLiquidacion();
             Neg_Cupon logic = new Neg_Cupon();
 
-            if (payload.RutCliente > 0)
+
+            var tecnocomCeco005WrapperClient = new TecnocomCeco005WrapperClient();
+            var prepagoDd = tecnocomCeco005WrapperClient.PrePagoDiezDias(payload.Cuenta, payload.Pan).FirstOrDefault();
+            if (prepagoDd != null)
             {
                 respuesta = await logic.ObtenerClienteCupon(respuesta, payload.RutCliente, payload.Canal);
-                resposneLiquidacion.PdfBuffer = await GenerarPdf(respuesta);
+                resposneLiquidacion.PdfBuffer = await GenerarPdf(respuesta, prepagoDd);
             }
             else
-                throw new InvalidEnumArgumentException("rut cliente");
+            {
+                throw new InvalidEnumArgumentException("No encontrado");
+            }
 
             return Ok(resposneLiquidacion);
         }
 
-        private string GenerarHtml(ResponseCliente respuesta)
+        private string GenerarHtml(ResponseCliente respuesta, Respuesta_CECO005_Registro_CECO005 registro)
         {
             return $"<html><body></body></html>";
         }
 
-        private async Task<byte[]> GenerarPdf(ResponseCliente respuesta)
+        private async Task<byte[]> GenerarPdf(ResponseCliente respuesta, Respuesta_CECO005_Registro_CECO005 registro)
         {
-            var htmlData = GenerarHtml(respuesta);
+            var htmlData = GenerarHtml(respuesta, registro);
             var result = await new GeneradorDocumentos().CrearDocumento(htmlData);
             return result;
         }
--- src/LiquidacionDeudaApi/ViewModels/RequestLiquidacion.cs
+++ src/LiquidacionDeudaApi/ViewModels/RequestLiquidacion.cs
@@ -7,6 +7,8 @@ namespace LiquidacionDeudaApi.ViewModels
 {
     public class RequestLiquidacion
     {
+        public string Cuenta { get; set; }
+        public string Pan { get; set; }
         public int RutCliente { get; set; }
         public int Canal { get; set; }
     }
